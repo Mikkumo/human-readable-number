033095942feb21801f5e5bac8552d1faf845852a
Lissa <sokolovskaya_lisa@mail.ru>
2022-02-28 16:40:37 +0400

    fix: relayout function toReadable

--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
-let digit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-let dozens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+let digit = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+let dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 let different = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 
 module.exports = function toReadable (number) {
-    return (number === 0) ? 'zero' : hungred(number)
+    return (number === 0) ? 'zero' : hundred(number)
 }
 
-function hungred (number) {
+function hundred (number) {
+    let getHundred = Math.floor(number / 100) - 1
+    let modFrom100 = number % 100
     if (number > 99) {
-        if (number % 10 === 0 && number % 100 === 0) {
-            return digit[Math.floor(number / 100)] + ' hundred'
-        } else if (number % 10 === 0) {
-            return digit[Math.floor(number / 100)] + ' hundred ' + dozens[Math.floor(number / 100) % 10]
-        } else {
-        return digit[Math.floor(number / 100)] + ' hundred ' + tens(number % 100)
-        }
+        if (modFrom100 === 0) return `${digit[getHundred]} hundred`
+        else if (modFrom100 % 10 === 0) return `${digit[getHundred]} hundred ${dozens[Math.floor(modFrom100 / 10) - 1]}`
+        else return `${digit[getHundred]} hundred ${tens(modFrom100)}`
     } else {
-        tens(number)
+        return tens(number)
     }
 } 
 
-function tens (number){
-    // if (number < 10) {
-    //     ones(number)
-    // } else 
-    if (number >=10 && number < 20) {
-        return different[number % 10]
+function tens (number) {
+    let getDozens = Math.floor(number / 10) - 1
+    let modFrom10 = number % 10
+    if (number >= 10 && number < 20) {
+        return different[modFrom10]
     } else if (number >= 20) {
-        return dozens[Math.floor(number / 10)] + ' ' + digit[number % 10]
-     } 
-    else {
-        digit[number]
-    }
-}
-
-function ones (number){
-    return digit[number]
+        if (modFrom10 === 0) return dozens[getDozens]
+        else return `${dozens[getDozens]} ${digit[modFrom10 - 1]}`
+    } else return digit[number - 1]
 }
